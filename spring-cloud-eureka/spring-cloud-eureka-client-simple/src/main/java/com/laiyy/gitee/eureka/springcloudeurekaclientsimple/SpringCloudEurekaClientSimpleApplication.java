47f070d12c78e45f815b811cf9f795d0d201d5d4
laiyy0728@gmail.com <danchun260.q>
2019-01-19 17:56:56 +0800

    regenerate pom regenerate module

--- spring-cloud-eureka/spring-cloud-eureka-client-simple/src/main/java/com/laiyy/gitee/eureka/springcloudeurekaclientsimple/SpringCloudEurekaClientSimpleApplication.java
+++ spring-cloud-eureka/spring-cloud-eureka-client-simple/src/main/java/com/laiyy/gitee/eureka/springcloudeurekaclientsimple/SpringCloudEurekaClientSimpleApplication.java
@@ -1,15 +1,21 @@
-package com.laiyy.gitee.springcloudeurekaclientsimple;
+package com.laiyy.gitee.eureka.springcloudeurekaclientsimple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author laiyy
+ * @infomation 一个简单的 Eureka Client
+ */
 @SpringBootApplication
-@EnableDiscoveryClient //@EnableEurekaClient
+@EnableDiscoveryClient // @EnableEurekaClient
 public class SpringCloudEurekaClientSimpleApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudEurekaClientSimpleApplication.class, args);
     }
+
 }
 
--- spring-cloud-eureka/spring-cloud-eureka-server-simple/src/main/java/com/laiyy/gitee/eureka/springcloudeurekaserversimple/SpringCloudEurekaServerSimpleApplication.java
+++ spring-cloud-eureka/spring-cloud-eureka-server-simple/src/main/java/com/laiyy/gitee/eureka/springcloudeurekaserversimple/SpringCloudEurekaServerSimpleApplication.java
@@ -1,14 +1,20 @@
-package com.laiyy.gitee.springcloudeurekaserver;
+package com.laiyy.gitee.eureka.springcloudeurekaserversimple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author laiyy
+ * @infomagtion 一个简单的 Eureka Server
+ */
 @SpringBootApplication
 @EnableEurekaServer
-public class SpringCloudEurekaServerApplication {
+public class SpringCloudEurekaServerSimpleApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
+        SpringApplication.run(SpringCloudEurekaServerSimpleApplication.class, args);
     }
+
 }
 
