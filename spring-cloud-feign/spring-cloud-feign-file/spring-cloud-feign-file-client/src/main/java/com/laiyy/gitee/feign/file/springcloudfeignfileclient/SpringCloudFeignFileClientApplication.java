cc490acbd0ecdc385c6b2dcedc2ed06dc86806e9
laiyy <=>
2019-01-24 17:36:06 +0800

    + feign file upload

---
+++ spring-cloud-feign/spring-cloud-feign-file/spring-cloud-feign-file-client/src/main/java/com/laiyy/gitee/feign/file/springcloudfeignfileclient/SpringCloudFeignFileClientApplication.java
@@ -0,0 +1,18 @@
+package com.laiyy.gitee.feign.file.springcloudfeignfileclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class SpringCloudFeignFileClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudFeignFileClientApplication.class, args);
+    }
+
+}
+
