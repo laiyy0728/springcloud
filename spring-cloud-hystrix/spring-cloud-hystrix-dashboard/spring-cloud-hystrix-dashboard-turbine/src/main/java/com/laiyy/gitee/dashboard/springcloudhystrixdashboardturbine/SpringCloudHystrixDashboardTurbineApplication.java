5714325c7b1da84e47a96eb959ddc843a3c8d4a3
laiyy <=>
2019-02-01 11:00:41 +0800

    + hystrix turbine

---
+++ spring-cloud-hystrix/spring-cloud-hystrix-dashboard/spring-cloud-hystrix-dashboard-turbine/src/main/java/com/laiyy/gitee/dashboard/springcloudhystrixdashboardturbine/SpringCloudHystrixDashboardTurbineApplication.java
@@ -0,0 +1,20 @@
+package com.laiyy.gitee.dashboard.springcloudhystrixdashboardturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableTurbine
+public class SpringCloudHystrixDashboardTurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudHystrixDashboardTurbineApplication.class, args);
+    }
+
+}
+
