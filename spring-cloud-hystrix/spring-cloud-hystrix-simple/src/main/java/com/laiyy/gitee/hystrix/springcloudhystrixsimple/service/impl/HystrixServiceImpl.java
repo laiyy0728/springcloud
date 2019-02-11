84f255268403a80d5251086c92d6dfa3fd9f526c
laiyy <=>
2019-02-11 11:31:22 +0800

    + hystrix config

--- spring-cloud-hystrix/spring-cloud-hystrix-simple/src/main/java/com/laiyy/gitee/hystrix/springcloudhystrixsimple/service/impl/HystrixServiceImpl.java
+++ spring-cloud-hystrix/spring-cloud-hystrix-simple/src/main/java/com/laiyy/gitee/hystrix/springcloudhystrixsimple/service/impl/HystrixServiceImpl.java
@@ -2,6 +2,9 @@ package com.laiyy.gitee.hystrix.springcloudhystrixsimple.service.impl;
 
 import com.laiyy.gitee.hystrix.springcloudhystrixsimple.service.HystrixService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.hystrix.contrib.javanica.conf.HystrixPropertiesManager;
+import org.springframework.cloud.netflix.hystrix.HystrixProperties;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,6 +14,11 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class HystrixServiceImpl implements HystrixService {
+///    @HystrixCommand(fallbackMethod = "defaultUser", commandProperties = {
+//            @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_STRATEGY, value = "THREAD")
+//    }, commandKey = "commandKey", threadPoolKey = "threadPoolKey", threadPoolProperties = {
+//            @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_THREAD_INTERRUPT_ON_TIMEOUT, value = "5000")
+//    })
     @Override
     @HystrixCommand(fallbackMethod = "defaultUser")
     public String getUser(String username) throws Exception {
