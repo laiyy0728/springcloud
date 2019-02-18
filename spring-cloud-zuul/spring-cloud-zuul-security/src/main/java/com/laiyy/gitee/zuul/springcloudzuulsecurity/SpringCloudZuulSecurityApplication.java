b3dc1888c1b41b54e33225435fa1827dceeb7d24
laiyy <=>
2019-02-18 15:26:59 +0800

    + zuul security

---
+++ spring-cloud-zuul/spring-cloud-zuul-security/src/main/java/com/laiyy/gitee/zuul/springcloudzuulsecurity/SpringCloudZuulSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.laiyy.gitee.zuul.springcloudzuulsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudZuulSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudZuulSecurityApplication.class, args);
+    }
+
+}
