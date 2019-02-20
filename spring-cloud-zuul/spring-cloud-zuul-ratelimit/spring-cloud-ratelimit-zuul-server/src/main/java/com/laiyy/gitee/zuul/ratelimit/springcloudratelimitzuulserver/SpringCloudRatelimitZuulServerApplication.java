097edc17a2b6a3faa5f54be2907094830ba72a93
laiyy <=>
2019-02-20 14:24:37 +0800

    + zuul ratelimit

---
+++ spring-cloud-zuul/spring-cloud-zuul-ratelimit/spring-cloud-ratelimit-zuul-server/src/main/java/com/laiyy/gitee/zuul/ratelimit/springcloudratelimitzuulserver/SpringCloudRatelimitZuulServerApplication.java
@@ -0,0 +1,13 @@
+package com.laiyy.gitee.zuul.ratelimit.springcloudratelimitzuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudRatelimitZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudRatelimitZuulServerApplication.class, args);
+    }
+
+}
