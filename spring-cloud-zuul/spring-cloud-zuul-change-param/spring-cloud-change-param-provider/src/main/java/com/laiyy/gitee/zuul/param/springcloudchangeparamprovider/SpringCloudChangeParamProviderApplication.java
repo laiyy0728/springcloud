297069d56c13fd9b8db00c8dec370a58e48aa41c
laiyy0728 <=>
2019-02-21 17:31:38 +0800

    + zuul change param

---
+++ spring-cloud-zuul/spring-cloud-zuul-change-param/spring-cloud-change-param-provider/src/main/java/com/laiyy/gitee/zuul/param/springcloudchangeparamprovider/SpringCloudChangeParamProviderApplication.java
@@ -0,0 +1,13 @@
+package com.laiyy.gitee.zuul.param.springcloudchangeparamprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudChangeParamProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudChangeParamProviderApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-zuul/spring-cloud-zuul-change-param/spring-cloud-change-param-zuul-server/src/main/java/com/laiyy/gitee/zuul/param/springcloudchangeparamzuulserver/SpringCloudChangeParamZuulServerApplication.java
@@ -0,0 +1,13 @@
+package com.laiyy.gitee.zuul.param.springcloudchangeparamzuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudChangeParamZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudChangeParamZuulServerApplication.class, args);
+    }
+
+}
