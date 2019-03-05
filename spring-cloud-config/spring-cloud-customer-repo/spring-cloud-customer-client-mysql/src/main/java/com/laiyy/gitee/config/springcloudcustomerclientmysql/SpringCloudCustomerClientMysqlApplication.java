58c6cfc976efa19b0b31e44a5db13a3610c3797e
laiyy0728 <=>
2019-03-05 17:03:01 +0800

    + spring cloud customer repo mysql

---
+++ spring-cloud-config/spring-cloud-customer-repo/spring-cloud-customer-client-mysql/src/main/java/com/laiyy/gitee/config/springcloudcustomerclientmysql/SpringCloudCustomerClientMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.laiyy.gitee.config.springcloudcustomerclientmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCustomerClientMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudCustomerClientMysqlApplication.class, args);
+    }
+
+}
