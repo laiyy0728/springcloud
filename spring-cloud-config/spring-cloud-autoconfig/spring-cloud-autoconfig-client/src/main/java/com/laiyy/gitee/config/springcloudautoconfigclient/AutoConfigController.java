a6696ed3119dbe44d770a6756136e2c7ae337730
laiyy0728 <=>
2019-03-06 16:15:13 +0800

    + spring cloud client auto refresh

---
+++ spring-cloud-config/spring-cloud-autoconfig/spring-cloud-autoconfig-client/src/main/java/com/laiyy/gitee/config/springcloudautoconfigclient/AutoConfigController.java
@@ -0,0 +1,25 @@
+package com.laiyy.gitee.config.springcloudautoconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author laiyy
+ * @date 2019/3/6 15:07
+ * @description
+ */
+@RestController
+@RefreshScope
+public class AutoConfigController {
+
+    @Value("${com.laiyy.gitee.config}")
+    private String config;
+
+    @GetMapping(value = "/get-config-info")
+    public String getConfigInfo(){
+        return config;
+    }
+
+}
---
+++ spring-cloud-config/spring-cloud-autoconfig/spring-cloud-autoconfig-client/src/main/java/com/laiyy/gitee/config/springcloudautoconfigclient/SpringCloudAutoconfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.laiyy.gitee.config.springcloudautoconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAutoconfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAutoconfigClientApplication.class, args);
+    }
+
+}
