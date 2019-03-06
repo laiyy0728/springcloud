19974b320f6fea718f4d6b1b9e6d7cfa08ac6c64
laiyy0728 <=>
2019-03-06 14:35:17 +0800

    + spring cloud customer repo mongodb

---
+++ spring-cloud-config/spring-cloud-customer-repo/spring-cloud-customer-client-mongo/src/main/java/com/laiyy/gitee/config/springcloudcustomerclientmongo/ConfigInfoProperties.java
@@ -0,0 +1,23 @@
+package com.laiyy.gitee.config.springcloudcustomerclientmongo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author laiyy
+ * @date 2019/3/4 14:09
+ * @description
+ */
+@Component
+@ConfigurationProperties(prefix = "com.laiyy.gitee")
+public class ConfigInfoProperties {
+    private String config;
+
+    public String getConfig() {
+        return config;
+    }
+
+    public void setConfig(String config) {
+        this.config = config;
+    }
+}
---
+++ spring-cloud-config/spring-cloud-customer-repo/spring-cloud-customer-client-mongo/src/main/java/com/laiyy/gitee/config/springcloudcustomerclientmongo/SpringCloudCustomerClientMongoApplication.java
@@ -0,0 +1,13 @@
+package com.laiyy.gitee.config.springcloudcustomerclientmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCustomerClientMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudCustomerClientMongoApplication.class, args);
+    }
+
+}
