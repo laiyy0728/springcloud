f311d3cc7645188c10512b751843426b1cfb2e42
laiyy0728@gmail.com <danchun260.q>
2019-04-07 16:18:02 +0800

    config client ha

---
+++ spring-cloud-config/spring-cloud-config-ha/spring-cloud-config-ha-client/spring-cloud-config-ha-client-config/src/main/java/com/laiyy/gitee/config/springcloudconfighaclientconfig/SpringCloudConfigHaClientConfigApplication.java
@@ -0,0 +1,13 @@
+package com.laiyy.gitee.config.springcloudconfighaclientconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigHaClientConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigHaClientConfigApplication.class, args);
+    }
+
+}
