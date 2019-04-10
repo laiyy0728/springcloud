2594a3dc7145c9271ccff261c72904da826fa283
laiyy0728 <laiyy0728@gmail.com>
2019-04-11 00:21:25 +0800

    apm sleuth

---
+++ spring-cloud-apm/spring-cloud-apm-sleuth/spring-cloud-apm-sleuth-consumer/src/main/java/com/laiyy/gitee/apm/springcloudapmsleuthconsumer/SpringCloudApmSleuthConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.laiyy.gitee.apm.springcloudapmsleuthconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApmSleuthConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudApmSleuthConsumerApplication.class, args);
+    }
+
+}
