78e1e5400af01045c96dde338fad6d6a365c98a1
laiyy0728 <laiyy0728@gmail.com>
2019-04-11 00:21:25 +0800

    apm sleuth

---
+++ spring-cloud-apm/spring-cloud-apm-sleuth/spring-cloud-apm-sleuth-provider/src/main/java/com/laiyy/gitee/apm/springcloudapmsleuthprovider/SpringCloudApmSleuthProviderApplication.java
@@ -0,0 +1,13 @@
+package com.laiyy.gitee.apm.springcloudapmsleuthprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApmSleuthProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudApmSleuthProviderApplication.class, args);
+    }
+
+}
